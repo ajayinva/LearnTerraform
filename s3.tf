5f1a78f7813517bbac76fcebb10538ac133df32f
Ajay Agarwal <ajayinva@gmail.com>
2018-12-07 16:26:43 -0500

    Added variables

--- s3.tf
+++ s3.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "main" {
-  bucket = "ajay-learn-terraform"
+  bucket = "${var.s3_bucket_name}"
   acl    = "private"
+  tags = {
+  	env = "${lookup(var.s3_tags,"environment")}"
+  }
+  region = "${var.s3_regions[0]}"
 }
